2f72bab9449841853b9c9a16e3244f08a9cff2e4
GMatrixGames <gmatrixgames@hotmail.com>
2021-09-22 14:22:55 -0400

    More types

---
+++ FN/Enums/Fortnite/ELayeredAudioTriggerDirection.cs
@@ -0,0 +1,11 @@
+namespace CUE4Parse.FN.Enums.Fortnite
+{
+    public enum ELayeredAudioTriggerDirection : byte
+    {
+        AnyDirection = 0,
+        Forwards = 1,
+        Sideways = 2,
+        Backwards = 3,
+        Count = 4
+    }
+}
